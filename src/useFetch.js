2200fe1bcc393171dc7c0fad68fe32bfb4254108
Leonardo Malave <leomalave1993@gmail.com>
2022-12-08 15:00:15 -0400

    reusing custom hooks

--- src/useFetch.js
+++ src/useFetch.js
@@ -24,7 +24,7 @@ const useFetch = (url) => {
                 setIsPending(false);
                 setError(err.message);
               })
-        }, 1000);
+        }, 250);
      }, []);
 
      return { data, isPending, error }
