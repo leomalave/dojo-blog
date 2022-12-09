5bf8bb24c0f92549f26bd77ff79ce4443bb4a383
Leonardo Malave <leomalave1993@gmail.com>
2022-12-09 14:56:13 -0400

    making post request

--- src/Create.js
+++ src/Create.js
@@ -1,23 +1,54 @@
+import { useState } from "react";
+
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('yoshi');
+    
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = { title, body, author };
+    
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json"},
+            body: JSON.stringify(blog)
+        })
+      }
+
+
     return ( 
         <div className="create">
             <h2>Add a New Blog</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Blog title:</label>
                 <input 
                     type="text"
-                    required    
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
                 />
                 <label>Blog title:</label>
                 <textarea
                 required
+                value={body}
+                onChange={(e) => setBody(e.target.value)}
                 ></textarea>
                 <label>Blog author:</label>
-                <select>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
                     <option value="mario">mario</option>
                     <option value="yoshi">yoshi</option>
                 </select>
                 <button>Add Blog</button>
+                {/* <p>{ title }</p>
+                <p>{ body }</p>
+                <p>{ author }</p> */}
+
             </form>
         </div>
      );
